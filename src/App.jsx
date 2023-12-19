d9723d04c8150cd3debbdd9001a419f3f3c10e14
Sanjaykumar Gouda <sanjay.kumar@oropocket.com>
2023-12-19 18:46:49 +0530

    feat(navigation:using react-router-dom)

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import ProductList from "./components/ProductList";
 import Navbar from "./components/Shared/Navbar";
 import { Provider } from "react-redux";
 import store from "./store";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Provider store={store}>
         <Navbar />
-        <ProductList />
+        <Routes>
+          <Route element={<ProductList />} path="/" />
+          <Route element={<ProductDetail />} path="/product-details" />
+        </Routes>
       </Provider>
 
       {/* <ProductDetail /> */}
